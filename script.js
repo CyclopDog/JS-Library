3a434552b2c35bbc84326384e78131100746c83f
erib <eri-b@users.noreply.github.com>
2019-08-23 14:36:24 -0400

    form, delete buttons

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let bookList = document.getElementById("book-list");
-let myLibrary = ['Lord OF The Rings', 'The Hobbit', 'Rails Tutorial by Michael'];
-
-function Book() {
-  // the constructor...
+function Book(title, author, num_pages, id) {
+  this.title = title;
+  this.author = author;
+  this.num_pages = num_pages;
+  this.id = `b${id}`;
 }
+
+book1 = new Book("Lord of the Rings", "JR T", 999, 1);
+book2 = new Book("The Hobbit", "JR T", 499, 2);
+book3 = new Book("Rails Tutorial", "Michael Hartl", 299, 3);
+
+let myLibrary = [book1, book2, book3];
 
 function addBookToLibrary(book) {
   // do stuff here
@@ -11,9 +17,21 @@ function addBookToLibrary(book) {
 }
 
 function render(arr) {
+    let bookList = document.querySelector("#book-list");
     arr.forEach(book => {
-        bookList.innerHTML += '<li>' + book + '</li>';
+        bookList.innerHTML += `<li>${book.title}</li>`;
+        bookList.innerHTML += `<button id=${book.id}>Delete</button>`
+        document.querySelector(`#${book.id}`).addEventListener('click', event => {
+          //delete something
+        });
     });
 }
+
+document.querySelector("#new").addEventListener('click', event => {
+  document.querySelector("#book_form").style.display = "block";
+});
+
+
+
 
 render(myLibrary);
